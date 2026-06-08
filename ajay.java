cc12376cad5d912c4f1a616fb9694981b7b7f63c
root <root@ip-172-31-36-7.ec2.internal>
2026-06-08 07:06:45 +0000

    java-code

---
+++ ajay.java
@@ -0,0 +1,8 @@
+$ cat ajay.java
+public class ajay {
+        public static void main (string[] args){
+                int a=10, b=20;
+                system.out.println("Addition is:"+(a+b));
+        }
+}
+
